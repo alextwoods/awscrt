bc5c6ef7d7bdac1c21a8b5245bf4d6f4e71ed5ba
Alex Woods <alexwoo@amazon.com>
2024-05-09 13:11:44 -0700

    Use cbor branch of aws-c-common

--- lib/awscrt.rb
+++ lib/awscrt.rb
@@ -2,8 +2,43 @@
 
 require_relative "awscrt/version"
 require_relative "awscrt/awscrt"
+require_relative 'awscrt/cbor/cbor_engine'
 
 module Awscrt
   class Error < StandardError; end
   # Your code goes here...
+
+  def self.cbor_encode(value)
+    encoder = Awscrt::CborEncoder.new
+    encoder.add(value)
+    encoder.encoded_data
+  end
+
+  class CborEncoder
+    def add(value)
+      case value
+      when Integer
+        if value < 0
+          add_neg_int(-1 - value)
+        else
+          add_pos_int(value)
+        end
+      when Numeric then add_auto_float(value)
+      when Symbol then add_string(value.to_s)
+      when String then add_string(value)
+      when Array
+        start_array(value.size)
+        value.each { |di| add(di) }
+      when Hash
+        start_map(value.size)
+        value.each do |k, v|
+          add(k)
+          add(v)
+        end
+      else
+        raise UnknownTypeError, value
+      end
+      self
+    end
+  end
 end
